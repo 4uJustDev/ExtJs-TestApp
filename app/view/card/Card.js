1dbdef5848782c1f65b268b8a0f9fd0b0302e0fb
Alexandr Zarubin <zarubin_14@list.ru>
2024-01-25 15:53:51 +0300

    ESLINTCard.js

--- app/view/card/Card.js
+++ app/view/card/Card.js
@@ -1,58 +1,58 @@
 Ext.define('MyApp.view.card.Card', {
-    extend: 'Ext.Window',
-    xtype: 'itemcard',
+  extend: 'Ext.Window',
+  xtype: 'itemcard',
 
-    controller: 'card-panel',
+  controller: 'card-panel',
 
-    requires: [
-        'MyApp.view.login.LoginController',
-        'Ext.form.Panel'
-    ],
+  requires: [
+    'MyApp.view.login.LoginController',
+    'Ext.form.Panel'
+  ],
 
-    id: 'itemCard',
-    title: 'Карточка товара',
-    bodyPadding: 20,
+  id: 'itemCard',
+  title: 'Карточка товара',
+  bodyPadding: 20,
 
+  items: [{
+    xtype: 'form',
+    id: 'itemForm',
     items: [{
-        xtype: 'form',
-        id: 'itemForm',
-        items: [{
-            xtype: 'field',
-            fieldLabel: 'ID',
-            name: 'id',
-            inputType: 'string',
-            allowBlank: false,
-        }, {
-            xtype: 'field',
-            fieldLabel: 'Наименование',
-            width: 300,
-            name: 'name',
-            inputType: 'string'
-        }, {
-            xtype: 'numberfield',
-            fieldLabel: 'Цена',
-            minValue: 0,
-            allowDecimals: true,
-            incrementValue: 0.5,
-            name: 'price'
-        }, {
-            xtype: 'numberfield',
-            fieldLabel: 'Количество',
-            name: 'amount',
-            minValue: 0,
-            stepValue: 1,
-            maxValue: 100,
-        }],
-        buttons: [{
-            text: 'Сохранить',
-            listeners: {
-                click: 'clickOnConfirmCard'
-            }
-        }, {
-            text: 'Отмена',
-            listeners: {
-                click: 'clickOnCancelCard'
-            }
-        }]
+      xtype: 'field',
+      fieldLabel: 'ID',
+      name: 'id',
+      inputType: 'string',
+      allowBlank: false,
+    }, {
+      xtype: 'field',
+      fieldLabel: 'Наименование',
+      width: 300,
+      name: 'name',
+      inputType: 'string'
+    }, {
+      xtype: 'numberfield',
+      fieldLabel: 'Цена',
+      minValue: 0,
+      allowDecimals: true,
+      incrementValue: 0.5,
+      name: 'price'
+    }, {
+      xtype: 'numberfield',
+      fieldLabel: 'Количество',
+      name: 'amount',
+      minValue: 0,
+      stepValue: 1,
+      maxValue: 100,
+    }],
+    buttons: [{
+      text: 'Сохранить',
+      listeners: {
+        click: 'clickOnConfirmCard'
+      }
+    }, {
+      text: 'Отмена',
+      listeners: {
+        click: 'clickOnCancelCard'
+      }
     }]
+  }]
 });
