af5259a51611a27a6ad6be0cdb6d63983edd4639
Alexandr Zarubin <zarubin_14@list.ru>
2024-01-25 15:54:22 +0300

    ESLIN Product.js Model

--- app/model/Product.js
+++ app/model/Product.js
@@ -1,11 +1,11 @@
 Ext.define('MyApp.model.Product', {
-    extend: 'MyApp.model.Base',
+  extend: 'MyApp.model.Base',
 
-    fields: [
-        {id:'id', type: 'int'}, 
+  fields: [
+    {id:'id', type: 'int'}, 
 	    {name: 'name', type: 'string'},
 	    {description:'description', type: 'string'},
 	    {price: 'price', type: 'int'},
 	    {amount: 'amount', type: 'int'}
-    ]
+  ]
 });
