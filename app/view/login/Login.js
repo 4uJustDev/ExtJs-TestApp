f3a8bc98bc071dfca754ff67953faf74d97b8938
Alexandr Zarubin <zarubin_14@list.ru>
2024-01-25 15:49:54 +0300

    ESLINTLogin.js

--- app/view/login/Login.js
+++ app/view/login/Login.js
@@ -1,43 +1,43 @@
 Ext.define('MyApp.view.login.Login', {
-    extend: 'Ext.window.Window',
-    xtype: 'login',
+  extend: 'Ext.window.Window',
+  xtype: 'login',
 
-    requires: [
-        'MyApp.view.login.LoginController',
-        'Ext.form.Panel'
-    ],
+  requires: [
+    'MyApp.view.login.LoginController',
+    'Ext.form.Panel'
+  ],
 
-    controller: 'login',
-    bodyPadding: 10,
-    title: 'Login Window',
-    closable: false,
-    autoShow: true,
+  controller: 'login',
+  bodyPadding: 10,
+  title: 'Login Window',
+  closable: false,
+  autoShow: true,
 
-    items: {
-        xtype: 'form',
-        reference: 'form',
-        items: [{
-            xtype: 'textfield',
-            name: 'username',
-            fieldLabel: 'Username',
-            allowBlank: false
-        }, {
-            xtype: 'textfield',
-            name: 'password',
-            inputType: 'password',
-            fieldLabel: 'Password',
-            allowBlank: false
-        }, {
-            xtype: 'displayfield',
-            hideEmptyLabel: false,
-            value: 'Enter any non-blank password'
-        }],
-        buttons: [{
-            text: 'Login',
-            formBind: true,
-            listeners: {
-                click: 'onLoginClick'
-            }
-        }]
-    }
+  items: {
+    xtype: 'form',
+    reference: 'form',
+    items: [{
+      xtype: 'textfield',
+      name: 'username',
+      fieldLabel: 'Username',
+      allowBlank: false
+    }, {
+      xtype: 'textfield',
+      name: 'password',
+      inputType: 'password',
+      fieldLabel: 'Password',
+      allowBlank: false
+    }, {
+      xtype: 'displayfield',
+      hideEmptyLabel: false,
+      value: 'Enter any non-blank password'
+    }],
+    buttons: [{
+      text: 'Login',
+      formBind: true,
+      listeners: {
+        click: 'onLoginClick'
+      }
+    }]
+  }
 });
