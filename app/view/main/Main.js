af392c497dd36640c62c7c5b7f6ba7fc5a0ea7fe
Alexandr Zarubin <zarubin_14@list.ru>
2024-01-25 15:49:00 +0300

    ESLINT Main.js

--- app/view/main/Main.js
+++ app/view/main/Main.js
@@ -1,64 +1,64 @@
 Ext.define('MyApp.view.main.Main', {
-    extend: 'Ext.panel.Panel',
-    xtype: 'app-main',
+  extend: 'Ext.panel.Panel',
+  xtype: 'app-main',
 
-    requires: [
-        'Ext.plugin.Viewport',
-        'MyApp.view.main.MainController',
-    ],
+  requires: [
+    'Ext.plugin.Viewport',
+    'MyApp.view.main.MainController',
+  ],
 
-    controller: 'main',
-    viewModel: {
-        type: 'main'
+  controller: 'main',
+  viewModel: {
+    type: 'main'
+  },
+  plugins: 'viewport',
+
+  layout: 'fit',
+  viewModel: true,
+
+  tbar: [{
+    xtype: 'label',
+    text: 'Учет товаров:',
+    padding: '0 0 0 5',
+  }, {
+    xtype: 'button',
+    text:'Добавить',
+    glyph: 43,
+    tooltip: 'Товары',
+    listeners: {
+      click: 'onAddTabClick'
+    }
+  }, {
+    xtype: 'button',
+    text: 'Выход',
+    tooltip: 'Выйти из аккаунта!',
+    listeners: {
+      click: 'onClickButtonDestroy'
     },
-    plugins: 'viewport',
+  }],
 
-    layout: 'fit',
-    viewModel: true,
-
-    tbar: [{
-        xtype: 'label',
-        text: 'Учет товаров:',
-        padding: '0 0 0 5',
-    }, {
-        xtype: 'button',
-        text:'Добавить',
-        glyph: 43,
-        tooltip: 'Товары',
-        listeners: {
-            click: 'onAddTabClick'
-        }
-    }, {
-        xtype: 'button',
-        text: 'Выход',
-        tooltip: 'Выйти из аккаунта!',
-        listeners: {
-            click: 'onClickButtonDestroy'
-        },
-    }],
-
+  items: [{
+    xtype: 'tabpanel',
+    reference: 'tabpanel',
+    border: false,
+    defaults: {
+      bodyPadding: 0,
+      scrollable: true,
+      closable: true,
+      border: false
+    },
+    margin: '10',
+    tabPosition: 'top',
+    tabRotation: 'default',
     items: [{
-        xtype: 'tabpanel',
-        reference: 'tabpanel',
-        border: false,
-        defaults: {
-            bodyPadding: 0,
-            scrollable: true,
-            closable: true,
-            border: false
-        },
-        margin: '10',
-        tabPosition: 'top',
-        tabRotation: 'default',
-        items: [{
-            title: 'Tab 1',
-            xtype: 'mainlist'
-        }, {
-            title: 'Tab 2',
-            xtype: 'mainlist'
-        }, {
-            title: 'Tab 3',
-            xtype: 'mainlist'
-        }]
+      title: 'Tab 1',
+      xtype: 'mainlist'
+    }, {
+      title: 'Tab 2',
+      xtype: 'mainlist'
+    }, {
+      title: 'Tab 3',
+      xtype: 'mainlist'
     }]
+  }]
 });
