c5d3256765ad099fdb17f3c6a102dfccdbc31593
Alexandr Zarubin <zarubin_14@list.ru>
2024-01-25 15:47:22 +0300

    ESLINT Application.js

--- app/Application.js
+++ app/Application.js
@@ -1,40 +1,32 @@
 Ext.define('MyApp.Application', {
 
-    extend: 'Ext.app.Application',
+  extend: 'Ext.app.Application',
 
-    name: 'MyApp',
+  name: 'MyApp',
 
-    views: [
-        'MyApp.view.login.Login',
-        'MyApp.view.main.Main'
-    ],
+  views: [
+    'MyApp.view.login.Login',
+    'MyApp.view.main.Main'
+  ],
 
-    quickTips: false,
-    platformConfig: {
-        desktop: {
-            quickTips: true
-        }
-    },
-    
-    launch: function () {
-        // Переменная для проверки логина
-        var loggedIn;
-
-        //DEV
-        //localStorage.removeItem('TutorialLoggedIn');
-
-        loggedIn = localStorage.getItem("TutorialLoggedIn");
-
-        Ext.widget(loggedIn ? 'app-main' : 'login');
-    },
-
-    onAppUpdate: function () {
-        Ext.Msg.confirm('Application Update', 'This application has an update, reload?',
-            function (choice) {
-                if (choice === 'yes') {
-                    window.location.reload();
-                }
-            }
-        );
+  quickTips: false,
+  platformConfig: {
+    desktop: {
+      quickTips: true
     }
+  },
+    
+  launch: function () {
+    // Variable for check log in
+    let loggedIn;
+
+    //DEV
+    //localStorage.removeItem('TutorialLoggedIn');
+
+    // Getting the status of login
+    loggedIn = localStorage.getItem("TutorialLoggedIn");
+
+    // Show main window or login page
+    Ext.widget(loggedIn ? 'app-main' : 'login');
+  }
 });
